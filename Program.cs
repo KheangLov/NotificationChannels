270ddf724d1b9326ef1f57d8d8290af08cb7e068
KheangLov <lovsokheang@gmail.com>
2024-09-19 10:42:13 +0700

    First project initialize

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+int Case = 1;
+
+while (true)
+{
+    Console.WriteLine($"Case {Case}");
+    Console.Write("\t• Input: ");
+
+    string Notification = Console.ReadLine()!;
+
+    string[] DefaultChannels = { "BE", "FE", "QA", "Urgent" };
+    string[] Channels = Notification!.Split('[', ']').Where(Value => DefaultChannels.Contains(Value)).ToArray();
+    string Channel = String.Join(", ", Channels);
+
+    Console.WriteLine($"\t• Output: Receive channels: {Channel}");
+
+    Console.Write("\nDo you want to reinput? (y/n): ");
+
+    string Reinput = Console.ReadLine()!;
+    if (Reinput.Trim().ToLower() != "y")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine();
+        Case++;
+    }
+}
